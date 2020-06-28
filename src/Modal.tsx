861c588a9a5ca5eaf3edba9a8bdef73dfe3bc6e4
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-06-28 21:31:54 +0800

    内联css类的样式

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,10 +1,11 @@
 import "./Modal.css";
 import React from "react";
+import ImageView from "./ImageView";
 export interface ModalProps {
     imgSrc?: string,
     React: any,
-    opacity:boolean,
-    handleChangeOpacity:(newOpacity:boolean,imgSrc?:string)=>void
+    opacity: boolean,
+    handleChangeOpacity: (newOpacity: boolean, imgSrc?: string) => void
 }
 
 
@@ -17,15 +18,31 @@ export class Modal extends React.Component<ModalProps>{
 
     render() {
         const React = this.React
-        const opacity=this.props.imgSrc && this.props.opacity
-        return (<div className="modal" 
-        onClick={()=>this.props.handleChangeOpacity(!this.props.opacity,this.props.imgSrc)} 
-        style={{ opacity:opacity ? 1 : 0,visibility:opacity ? 'visible' : 'hidden' }} 
->
-            <div className="modal-body">
-                <img src={this.props.imgSrc} width='100%' height='100%'  />
+        const opacity = this.props.imgSrc && this.props.opacity
+        return (<div
+            onClick={() => this.props.handleChangeOpacity(!this.props.opacity, this.props.imgSrc)}
+            style={{
+                opacity: opacity ? 1 : 0,
+                visibility: (opacity ? 'visible' : 'hidden'),
+                position: 'fixed',
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                width: '100%',
+                zIndex: 9999,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: ' rgba(0, 0, 0, 0.50)',
+                transition: ' all 550ms ease-in '
+            }
+            }
+        >
+            <div style={{ margin: '0.8rem' }}>
+                <ImageView src={this.props.imgSrc} width='100%' height='100%' />
             </div>
-        </div>
+        </div >
 
     /* 作者：heibaimeng
     链接：https://juejin.im/post/5cf3d3ba5188257c6b5171fd
