e42e4e61d661ff21f9f400a68aa1e1df74289b50
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-11-07 18:32:10 +0800

    Delete util.ts

--- src/util.ts
+++
@@ -1,7 +0,0 @@
-export function Iterator2Array<T>(iterator: IterableIterator<T>): Array<T> {
-    const array: Array<T> = []
-    for (const item of iterator) {
-        array.push(item)
-    }
-    return array
-}
