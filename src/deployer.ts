020ce76f0a5c0ea4b0021796f76aae6111d6128a
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-10-02 00:11:35 +0800

    更改测试

--- src/deployer.ts
+++ src/deployer.ts
@@ -1,14 +1,12 @@
 import { attachListeners, showModal, setContainer, setImageModal } from "./deploy"
 import { ImageModalWithEXIF } from "./ImageModalWithEXIF"
-(() => {
-    const node =document.createElement('div')
-    document.body.appendChild(node)
-    setImageModal(ImageModalWithEXIF)
-    setContainer(node)
-    if (process.env.NODE_ENV === 'development') {
-        attachListeners(document.querySelectorAll('img'))
-    } else {
-        attachListeners(document.querySelectorAll('div.entry-content img:not(.avatar)'))
-    }
-    showModal()
-})()
+const node = document.createElement('div')
+document.body.appendChild(node)
+setImageModal(ImageModalWithEXIF)
+setContainer(node)
+if (process.env.NODE_ENV === 'development') {
+    attachListeners(document.querySelectorAll('img'))
+} else {
+    attachListeners(document.querySelectorAll('div.entry-content img:not(.avatar)'))
+}
+showModal()
